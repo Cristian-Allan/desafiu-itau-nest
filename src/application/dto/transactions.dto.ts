cb626db11d9e8b91176dee892a5111abe6ad344f
Cristian-Allan <allancristian787@gmail.com>
2026-05-02 16:13:29 -0300

    feat: simple update in TransactionDTO

--- src/application/dto/transactions.dto.ts
+++ src/application/dto/transactions.dto.ts
@@ -1,7 +1,6 @@
 import { IsDateString, IsNotEmpty, IsNumber, Min } from "class-validator";
 
 export class TransactionDTO {
-
     @IsNotEmpty({ message: 'O valor da transação é obrigatório.' })
     @IsNumber({}, { message: 'O valor da transação deve ser um número.' })
     @Min(0.01, { message: 'O valor da transação não pode ser negativo.' })
@@ -10,4 +9,9 @@ export class TransactionDTO {
     @IsNotEmpty({ message: 'A data e hora da transação são obrigatórias.' })
     @IsDateString({}, { message: 'A data e hora da transação não são válidas.' })
     dataHora: string;
+
+    constructor(valor: number, dataHora: string) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
 }
